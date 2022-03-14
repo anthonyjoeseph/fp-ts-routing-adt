540cb98cd1dcd6e408e0657af7a66b7833e86bb3
Anthony Gabriele <anthonyjoeseph@gmail.com>
2022-03-14 03:31:42 -0400

    jsdoc

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 import type { Match, Parser, Formatter } from 'fp-ts-routing';
 import { parse, zero, Route, format, end } from 'fp-ts-routing';
 
+/**
+ * Parses route strings to a sum type using Matches from fp-ts-routing.
+ * Note: Formatting the `NotFound` type will return '/'
+ * 
+ * @example
+ * import * as R from 'fp-ts-routing'
+ * import { routingFromMatches } from 'fp-ts-routing-adt'
+ * 
+ * const landing = R.end
+ * const show = R.lit('show').then(R.end)
+ * const id = R.int('id').then(R.end)
+ * 
+ * const { parse, format } = routingFromMatches(
+ *   ['Landing', landing],
+ *   ['Show', show],
+ *   ['Id', id],
+ * );
+ * type RouteADT = ReturnType<typeof parse>
+ * 
+ * @param pairs An list of [string, Match] tuples
+ * @returns A 'parse' and a 'format' function
+ */
 export function routingFromMatches<
   // Using a separate 'Keys' type gives us inference
   Keys extends string[],
