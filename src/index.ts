d35d1e6b7b772891e7eaec6ed8527dfec9fdf213
Anthony Gabriele <anthonyjoeseph@gmail.com>
2022-03-14 03:28:09 -0400

    pick imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import * as R from 'fp-ts-routing';
+import type { Match, Parser, Formatter } from 'fp-ts-routing';
+import { parse, zero, Route, format, end } from 'fp-ts-routing';
 
 export function routingFromMatches<
   // Using a separate 'Keys' type gives us inference
@@ -7,28 +8,28 @@ export function routingFromMatches<
     [K in keyof Keys]: 
     [
       Keys[K], 
-      Omit<R.Match<{ [s: string]: unknown }>, "formatter"> 
-        & { formatter: { run: (r: R.Route, a: never) => R.Route } }
+      Omit<Match<{ [s: string]: unknown }>, "formatter"> 
+        & { formatter: { run: (r: Route, a: never) => Route } }
     ] 
   },
 >(...pairs: Pairs) {
   type ADT = { type: 'NotFound' } | {
     [K in keyof Pairs]: 
-      Pairs[K] extends [infer Type, R.Match<infer Match>]
+      Pairs[K] extends [infer Type, Match<infer Match>]
         ? { type: Type } & Match
         : never
   }[number]
   return {
-    parse: (path: string): ADT => R.parse(
+    parse: (path: string): ADT => parse(
       pairs.reduce(
-        (acc, cur) => acc.alt(cur[1].parser.map(a => ({ type: cur[0], ...a })) as R.Parser<ADT>),
-        R.zero<ADT>(),
+        (acc, cur) => acc.alt(cur[1].parser.map(a => ({ type: cur[0], ...a })) as Parser<ADT>),
+        zero<ADT>(),
       ), 
-      R.Route.parse(path), 
+      Route.parse(path), 
       { type: 'NotFound' } as ADT
     ),
-    format: (adt: ADT): string => R.format(
-      pairs.find(([key]) => adt.type === key)?.[1]?.formatter as R.Formatter<ADT> ?? R.end.formatter as R.Formatter<{}>,
+    format: (adt: ADT): string => format(
+      pairs.find(([key]) => adt.type === key)?.[1]?.formatter as Formatter<ADT> ?? end.formatter as Formatter<{}>,
       adt
     ),
   };
