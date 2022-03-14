7207d204b7d7b830d1f3eff5188d22e5ba51e0a7
Anthony Gabriele <anthonyjoeseph@gmail.com>
2022-03-14 03:23:43 -0400

    separate 'keys' type

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import * as R from 'fp-ts-routing';
 
 export function routingFromMatches<
+  // Using a separate 'Keys' type gives us inference
   Keys extends string[],
-  Pairs extends { [K in keyof Keys]: [Keys[K], R.Match<any>] },
+  Pairs extends { 
+    [K in keyof Keys]: 
+    [
+      Keys[K], 
+      Omit<R.Match<{ [s: string]: unknown }>, "formatter"> 
+        & { formatter: { run: (r: R.Route, a: never) => R.Route } }
+    ] 
+  },
 >(...pairs: Pairs) {
   type ADT = { type: 'NotFound' } | {
     [K in keyof Pairs]: 
@@ -13,14 +21,14 @@ export function routingFromMatches<
   return {
     parse: (path: string): ADT => R.parse(
       pairs.reduce(
-        (acc, cur) => acc.alt(cur[1].parser.map(a => ({ type: cur[0], ...a }))),
+        (acc, cur) => acc.alt(cur[1].parser.map(a => ({ type: cur[0], ...a })) as R.Parser<ADT>),
         R.zero<ADT>(),
       ), 
       R.Route.parse(path), 
       { type: 'NotFound' } as ADT
     ),
     format: (adt: ADT): string => R.format(
-      pairs.find(([key]) => adt.type === key)?.[1]?.formatter ?? R.end.formatter,
+      pairs.find(([key]) => adt.type === key)?.[1]?.formatter as R.Formatter<ADT> ?? R.end.formatter as R.Formatter<{}>,
       adt
     ),
   };
